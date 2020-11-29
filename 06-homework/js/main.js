cdfd77ce351ee4a18d39f29bd007b7be7b8bfc26
Yuriy <leorentex@gmail.com>
2020-11-29 12:19:51 +0200

    create

---
+++ 06-homework/js/main.js
@@ -0,0 +1,16 @@
+const phone = {
+    display: 7.8,
+    weight: 0.284,
+    name: 'iphone',
+    price: 15000
+}
+
+console.log(phone);
+
+const phoneCopy = {...phone};
+
+console.log(phoneCopy);
+
+let test = prompt("sdasfdFSW", 0);
+
+'display' in phone;
