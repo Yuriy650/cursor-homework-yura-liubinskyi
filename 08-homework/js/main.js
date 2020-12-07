c4370e5104dc0beb8c083b51179ecb7843f5734c
Yuriy <leorentex@gmail.com>
2020-12-07 13:30:43 +0200

    changed

--- 08-homework/js/main.js
+++ 08-homework/js/main.js
@@ -5,60 +5,49 @@ class Student {
         this.fullName = fullName;
         this.marks = marks;
     }
-
     getAverageMark() {
         let count = this.marks.reduce((count, number) => {
             return count + number
         }, 0);
         return +(count / this.marks.length).toFixed(2);
     }
-
     dismiss() {
         return this.marks = null;
     }
-
     recover() {
         return this.marks = [];
     }
-
     studentsIndicate() {
         if (!this.marks) {
             return `Student left the univer`;
         }
         return `${this.fullName} come back to the university`;
     }
-
     static getInfo(university, course, fullName) {
         return `${fullName} - student ${course} course of ${university}`;
     }
-
     get getMarks() {
         return this.marks;
     }
-
     set setMarks(mark) {
         return this.marks = [...this.marks, mark];
     }
 }
-
 //1 Створіть новий клас BudgetStudent, який повністю наслідує клас Student
 class BudgetStudent extends Student {
     constructor(university, course, fullName, marks, scholarship) {
         super(university, course, fullName, marks);
         this.scholarship = scholarship;
     }
-
     getScholarship(scholarship) {
         console.log(`${this.fullName} received ${scholarship} uah scholarship`);
     }
-
     checkMark(scholarship) {
         if (this.getAverageMark() >= 4) {
             this.getScholarship(scholarship);
         } else console.log(`${this.fullName} go away!`);
     }
 }
-
 const sasha = new Student('Oxford University', 1, 'Oleksndr Ivaniv', [5, 5, 4, 4]);
 const galya = new Student('Politech University', 2, 'Galyna Stankiv', [4, 4, 5, 4]);
 const yura = new Student('Franko National University. of Lviv', 3, 'Yuriy Krups', [3, 4, 5, 3]);
@@ -83,10 +72,10 @@ console.log(BudgetStudent.getInfo(vitalik.university, vitalik.course, vitalik.fu
 //2.
 //3.
 vitalik.getScholarship(1000);
-let timer = setTimeout(function getMyScholarship() {
+/*let timer = setTimeout(function getMyScholarship() {
     ruslan.getScholarship(1000);
     timer = setTimeout(getMyScholarship, 10000);
-}, 10000);
+}, 10000);*/
 //4. Студент отримує стипендію тільки в тому випадку, якщо середній бал у нього вище або дорівнює 4.0
 anya.checkMark(2500);
 vitalik.checkMark(2500);
