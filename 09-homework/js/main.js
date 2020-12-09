446218762d36cc19eb24605f64575c9328d23ad3
Yuriy <leorentex@gmail.com>
2020-12-09 22:22:50 +0200

    changed

---
+++ 09-homework/js/main.js
@@ -0,0 +1,23 @@
+const generateBlocks = () => {
+    document.body.childNodes[0].remove();
+    const square = document.createElement('div');
+    square.classList.add('square');
+    document.body.prepend(square);
+    for (let i = 0; i < 5; i++) {
+        const tr = document.createElement('div');
+        for (let j = 0; j < 5; j++) {
+            const block = document.createElement('div');
+            block.classList.add('blocks');
+            tr.append(block);
+            block.style.background = getColor();
+        }
+        square.append(tr);
+    }
+}
+function getColor() {
+    return '#' + Math.random().toString(16).slice(5, 11);
+}
+function generateBlocksInterval() {
+    setInterval(generateBlocks, 1000);
+}
+
