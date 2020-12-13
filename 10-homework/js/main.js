48fdd1699eaac2f09d02f61cad16f6de00c72c8a
Yuriy <leorentex@gmail.com>
2020-12-13 20:16:18 +0200

    new

--- 10-homework/js/main.js
+++ 10-homework/js/main.js
@@ -1,3 +1,41 @@
-function helloWorld() {
-    console.log('Hello world!');
+function playSounds(name) {
+    let audio = new Audio();
+    audio.src = `sounds/${name}.mp3`;
+    audio.play();
 }
+let buttons = document.querySelectorAll('button');
+console.log([...buttons]);
+[...buttons].forEach((button) => {
+    document.addEventListener('keydown', function (e) {
+        if (e.code === 'Key' + button.textContent[0]) {
+            button.style.opacity = 0.5;
+            button.classList.add('push_button');
+            let fileName = button.textContent.slice(2);
+            playSounds(fileName);
+        }
+    });
+    document.addEventListener('keyup', function (e) {
+        if (e.code === 'Key' + button.textContent[0]) {
+            button.style.opacity = 1;
+            button.classList.remove('push_button');
+        }
+    });
+});
+document.addEventListener('mousedown', function (event) {
+    if (event.target.className === 'key_' + event.target.textContent[0]) {
+        let fileName = event.target.textContent.slice(2);
+        playSounds(fileName);
+        event.target.classList.add('push_button');
+    }
+})
+document.addEventListener('mouseup', function (event) {
+    event.target.classList.remove('push_button');
+})
+
+
+
+
+
+
+
+
