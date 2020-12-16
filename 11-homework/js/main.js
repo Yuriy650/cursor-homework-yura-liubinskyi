e1b6927aad04d76f1c308817fe8cd079a8ff8792
Yuriy <leorentex@gmail.com>
2020-12-17 01:30:41 +0200

    changed

--- 11-homework/js/main.js
+++ 11-homework/js/main.js
@@ -1,13 +1,15 @@
 async function getRandomChinese(length) {
     let chineseString = '';
+    const delay = 50;
+    const multiply = 1000;
     for (let i=0; i<length; i++) {
         let promise = new Promise(resolve => {
             setTimeout(() => {
                 let date = new Date();
-                const sign = date.getSeconds() * 1000 + date.getMilliseconds();
+                const sign = date.getSeconds() * multiply + date.getMilliseconds();
                 let chineseChar = String.fromCharCode(sign);
                 resolve(chineseChar);
-            }, 50);
+            }, delay);
         });
         await promise.then(chineseChar => chineseString = chineseString + ' ' + chineseChar);
             }
