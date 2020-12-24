cc79eb7fb2d75ae4d6f6f7bcbd6f2641ebdec8e3
Yuriy <leorentex@gmail.com>
2020-12-24 10:37:46 +0200

    new

---
+++ 13-homework/js/main.js
@@ -0,0 +1,33 @@
+function* createIdGenerator() {
+    yield 1;
+    yield 2;
+    yield 3;
+}
+
+const idGenerator = createIdGenerator();
+for (let value of idGenerator) {
+    console.log(value);
+}
+
+function* newFontGenerator(initialValue) {
+    let value = initialValue;
+    while (1) {
+        let font = yield value;
+        if (font === 'up') {
+            value = value + 2;
+
+        } else if (font === 'down') {
+            value = value - 2;
+        }
+    }
+}
+
+const fontGenerator = newFontGenerator(14);
+console.log(fontGenerator.next('up').value);
+console.log(fontGenerator.next('up').value);
+console.log(fontGenerator.next('up').value);
+console.log(fontGenerator.next('down').value);
+console.log(fontGenerator.next('down').value);
+console.log(fontGenerator.next('down').value);
+console.log(fontGenerator.next().value);
+document.querySelector('#changeFont').addEventListener('click', fontGenerator.next('up').value);
