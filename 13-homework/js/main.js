f6eb715aa8b191355dd1fb8443a43627c6ba84f7
Yuriy <leorentex@gmail.com>
2020-12-27 03:16:59 +0200

    change

--- 13-homework/js/main.js
+++ 13-homework/js/main.js
@@ -7,27 +7,28 @@ function* createIdGenerator() {
 const idGenerator = createIdGenerator();
 for (let value of idGenerator) {
     console.log(value);
+
 }
 
 function* newFontGenerator(initialValue) {
     let value = initialValue;
     while (1) {
         let font = yield value;
-        if (font === 'up') {
-            value = value + 2;
+        if (font === '+') {
+            value += 2;
 
-        } else if (font === 'down') {
-            value = value - 2;
+        } else if (font === '-') {
+            value -= 2;
         }
     }
 }
 
 const fontGenerator = newFontGenerator(14);
-console.log(fontGenerator.next('up').value);
-console.log(fontGenerator.next('up').value);
-console.log(fontGenerator.next('up').value);
-console.log(fontGenerator.next('down').value);
-console.log(fontGenerator.next('down').value);
-console.log(fontGenerator.next('down').value);
-console.log(fontGenerator.next().value);
+document.querySelector('#bigger_font').addEventListener('click', function () {
+    document.querySelector('#change_font').style.fontSize = `${fontGenerator.next('+').value}px`;
+});
+document.querySelector('#smaller_font').addEventListener('click', function () {
+    document.querySelector('#change_font').style.fontSize = `${fontGenerator.next('-').value}px`;
+})
+
 
